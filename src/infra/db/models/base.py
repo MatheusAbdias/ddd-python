7b1a6f776fc038f475faa1ce59180872f160982a
Matheus Abdias <matheusfabdias@gmail.com>
2024-10-06 17:55:18 -0300

    feat: :sparkles: create timestamped model

--- src/infra/db/models/base.py
+++ src/infra/db/models/base.py
@@ -1,5 +1,25 @@
-from sqlmodel import Field, SQLModel
+from datetime import datetime
+
+from sqlmodel import Column, DateTime, Field, SQLModel, func
 
 
 class Model(SQLModel):
     id: int | None = Field(default=None, primary_key=True)
+
+
+class TimeStampedModel(Model):
+    created_at: datetime | None = Field(
+        sa_column=Column(
+            DateTime(timezone=True),
+            nullable=False,
+            server_default=func.now(),
+        ),
+    )
+    updated_at: datetime | None = Field(
+        sa_column=Column(
+            DateTime(timezone=True),
+            nullable=False,
+            server_default=func.now(),
+            onupdate=func.now(),
+        ),
+    )
