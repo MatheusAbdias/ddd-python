d81d879cffac1b0b528108e4e85d6014976f46e9
Matheus Abdias <matheusfabdias@gmail.com>
2024-10-07 16:22:20 -0300

    feat: :sparkles: change accounts postgres repository

--- src/infra/db/repositories/accounts.py
+++ src/infra/db/repositories/accounts.py
@@ -1,18 +1,15 @@
 from dataclasses import dataclass
 
-from src.infra.db.models.accounts import Account, AccountType
-from src.infra.db.repositories.base import PostgresRepository
+from core.accounts.entities.enums import AccountType
+from infra.db.models.accounts import Account
+from infra.db.repositories.base import PostgresRepository
 
 
 @dataclass
 class PostgresAccountsRepository(PostgresRepository[Account]):
     model = Account
 
-    async def create_account(
-        self,
-        type: AccountType,
-        branch: str,
-        number: str,
-    ) -> Account:
-        account = Account(type=type, branch=branch, number=number)
+    async def create_account(self, type: AccountType) -> Account:
+        account = Account(type=type)
+
         return await self.insert(account)
